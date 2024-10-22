c12050a347fec2f715a161c7fa4f37abc341b18a
HoonJong KIM <uj06134@gmail.com>
2024-10-22 17:40:53 +0900

    cleaning

--- data_cleaning/ESG_cleaning_1.py
+++ data_cleaning/ESG_cleaning_1.py
@@ -1,10 +1,11 @@
-# 1. ESG 데이터 정제
-# ESG 데이터에서 필요한 컬럼만 추출
+# ESG 데이터 정제
+# 1) 컬럼명 변경
+# 2) 필요한 컬럼 추출
 
 import pandas as pd
 
 # CSV 파일 읽어오기
-result = pd.read_csv('../data_collection/data/esg_data/2024-ESG.csv')
+result = pd.read_csv('../data_collection/data/esg_data/2023-ESG.csv')
 
 # 컬럼명 변경
 result = result.rename(columns={
@@ -20,5 +21,5 @@ result = result[['회사명', '종합등급', '환경', '사회', '지배구조'
 
 print(result)
 
-# 변경된 DataFrame을 CSV 파일로 저장
-result.to_csv('cleaned_data/esg_data/2024-ESG-cleaned.csv', index=False)
+# 저장
+result.to_csv('cleaned_data/esg_data/2023-ESG-cleaned.csv', index=False)
