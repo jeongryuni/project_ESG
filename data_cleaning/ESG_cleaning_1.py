5b65f518eb002f51f5e1670489057c78064fde34
HoonJong KIM <uj06134@gmail.com>
2025-01-24 15:49:30 +0900

    add_2024

--- data_cleaning/ESG_cleaning_1.py
+++ data_cleaning/ESG_cleaning_1.py
@@ -5,7 +5,7 @@
 import pandas as pd
 
 # CSV 파일 읽어오기
-result = pd.read_csv('../data_collection/data/esg_data/2023-ESG.csv')
+result = pd.read_csv('../data_collection/data/esg_data/2024-ESG.csv')
 
 # 컬럼명 변경
 result = result.rename(columns={
@@ -22,4 +22,4 @@ result = result[['회사명', '종합등급', '환경', '사회', '지배구조'
 print(result)
 
 # 저장
-result.to_csv('cleaned_data/esg_data/2023-ESG-cleaned.csv', index=False)
+result.to_csv('cleaned_data/esg_data/2024-ESG-cleaned.csv', index=False)
